fb75bf507fba448fedc6ff693601101ca927321d
Crostantin <crostantin@gmail.com>
2014-11-19 16:01:18 +0100

    Add Status Bar menu
    - Power menu on status bar
    - Quick pulldown
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

---
+++ src/com/android/settings/boost/StatusBarSettings.java
@@ -0,0 +1,85 @@
+package com.android.settings.boost;
+
+import android.content.ContentResolver;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.text.format.DateFormat;
+import android.view.View;
+
+import com.android.settings.R;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+import com.android.settings.SettingsPreferenceFragment;
+
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.preference.PreferenceScreen;
+import android.provider.Settings.SettingNotFoundException;
+import com.android.settings.Utils;
+
+public class StatusBarSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_POWER_MENU = "status_bar_power_menu";
+    private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
+
+    private ListPreference mStatusBarPowerMenu;
+    private ListPreference mQuickPulldown;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.boost_status_bar_settings);
+
+        // status bar power menu
+        mStatusBarPowerMenu = (ListPreference) findPreference(STATUS_BAR_POWER_MENU);
+        mStatusBarPowerMenu.setOnPreferenceChangeListener(this);
+        int statusBarPowerMenu = Settings.System.getInt(getContentResolver(),
+                STATUS_BAR_POWER_MENU, 0);
+        mStatusBarPowerMenu.setValue(String.valueOf(statusBarPowerMenu));
+        mStatusBarPowerMenu.setSummary(mStatusBarPowerMenu.getEntry());
+
+	// quick settings pulldown
+	mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+	mQuickPulldown.setOnPreferenceChangeListener(this);
+	int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
+		Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);	
+	mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mStatusBarPowerMenu) {
+            String statusBarPowerMenu = (String) newValue;
+            int statusBarPowerMenuValue = Integer.parseInt(statusBarPowerMenu);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_POWER_MENU, statusBarPowerMenuValue);
+            int statusBarPowerMenuIndex = mStatusBarPowerMenu
+                    .findIndexOfValue(statusBarPowerMenu);
+            mStatusBarPowerMenu
+                    .setSummary(mStatusBarPowerMenu.getEntries()[statusBarPowerMenuIndex]);
+            return true;
+	} else if (preference == mQuickPulldown) {
+            int statusQuickPulldown = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    statusQuickPulldown);
+            return true;
+	}
+        return false;
+   }
+
+}
