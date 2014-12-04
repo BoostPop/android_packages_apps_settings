863c2ff485b8e1e303eeffb3dc5a15ca3c1a6f91
Crostantin <crostantin@gmail.com>
2014-12-04 04:24:05 +0100

    Settings: Do not allow to access quick settings on secure lockscreen - option (2/2)
    We have a lot security guys as user due of using the phones as business phones...that
    said a lot complains go into the direction of google how it is possible to have
    access to quick settings on a secure lockscreen which is absolutely understandable
    due that you are able to control Wifi BT AirplaneMode data etc.
    
    This patch takes care on this problem and adds an option to control this behaviour.
    
    ATTENTION: Due that this is really an UX derp for an average user this feature is
    enabled by default now. User need to disable it explicit in settings to get on secure
    locksreen the original android behaviour back.
    
    Change-Id: Iac8c181767a16d3d198672d16901670984b58f48
    
    original commit: https://github.com/SlimRoms/packages_apps_Settings/commit/a23400c0cfb37171cf547715a3bf04c24f812a28

--- src/com/android/settings/boost/SecurityTweaks.java
+++ src/com/android/settings/boost/SecurityTweaks.java
@@ -7,6 +7,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 
@@ -14,11 +15,14 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import com.android.internal.widget.LockPatternUtils;
+
 public class SecurityTweaks extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    // General
-    private static String STATUS_BAR_GENERAL_CATEGORY = "status_bar_general_category";
+    private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
+
+    SwitchPreference mBlockOnSecureKeyguard;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,9 +30,27 @@ public class SecurityTweaks extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.boost_security_tweaks);
 
+	PreferenceScreen prefs = getPreferenceScreen();
+
+	final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());	
+        mBlockOnSecureKeyguard = (SwitchPreference) findPreference(PREF_BLOCK_ON_SECURE_KEYGUARD);	
+        if (lockPatternUtils.isSecure()) {	
+            mBlockOnSecureKeyguard.setChecked(Settings.Secure.getInt(getContentResolver(),	
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 1) == 1);
+            mBlockOnSecureKeyguard.setOnPreferenceChangeListener(this);	
+        } else {	
+            prefs.removePreference(mBlockOnSecureKeyguard);	
+        }
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+	if (preference == mBlockOnSecureKeyguard) {	
+            Settings.Secure.putInt(getContentResolver(),	
+                    Settings.Secure.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,	
+                    (Boolean) objValue ? 1 : 0);	
+            return true;	
+	}
         return false;
     }
 
