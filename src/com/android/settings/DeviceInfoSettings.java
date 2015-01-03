678631fb5a559c49c5acb492a8eaed3516aa4b5b
Crostantin <crostantin@gmail.com>
2015-01-03 17:47:23 +0100

    Add boost version info (2/2)

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -82,6 +82,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_BOOST_VERSION = "boost_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -104,7 +105,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+	// let use boost version for a while setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+	setValueSummary(KEY_BOOST_VERSION, "ro.boost.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
