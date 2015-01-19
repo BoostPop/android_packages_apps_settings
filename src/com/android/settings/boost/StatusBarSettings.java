8cbac6ee7cb478cfc0ba5a9e24ac401655e33305
Crostantin <crostantin@gmail.com>
2015-01-19 21:20:10 +0100

    Status Bar Clock: rewrite for lollipop and add left clock (2/2) Add clock mods to cm-12.0 with new left position
    [Crostantin]: Adapt for Boostpop
    original: https://github.com/CyanogenMod/android_packages_apps_Settings/commit/323463f3737b627748ab642ee220f3fad0ad4f6c
    
    Change-Id: I3a53952a032b2241fa6dcca9b3a063b068af4ff4
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- src/com/android/settings/boost/StatusBarSettings.java
+++ src/com/android/settings/boost/StatusBarSettings.java
@@ -29,16 +29,25 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String STATUS_BAR_POWER_MENU = "status_bar_power_menu";
     private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
 
     private ListPreference mStatusBarPowerMenu;
     private ListPreference mQuickPulldown;
     private ListPreference mStatusBarBatteryShowPercent;
+    private ListPreference mStatusBarClock;
+    private ListPreference mStatusBarAmPm;
 
     @Override
+
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.boost_status_bar_settings);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+        mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
 
         mStatusBarBatteryShowPercent =
                 (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
@@ -63,16 +72,54 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 		Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);	
 	mQuickPulldown.setValue(String.valueOf(statusQuickPulldown));
 
+        int clockStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK, 1);
+        mStatusBarClock.setValue(String.valueOf(clockStyle));
+        mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        mStatusBarClock.setOnPreferenceChangeListener(this);
+
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        } else {
+            int statusBarAmPm = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_AM_PM, 2);
+            mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
+            mStatusBarAmPm.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        // Adjust clock position for RTL if necessary
+        Configuration config = getResources().getConfiguration();
+        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
+                        R.array.status_bar_clock_style_entries_rtl));
+                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mStatusBarPowerMenu) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mStatusBarClock) {
+            int clockStyle = Integer.parseInt((String) newValue);
+            int index = mStatusBarClock.findIndexOfValue((String) newValue);
+            Settings.System.putInt(
+                    resolver, STATUS_BAR_CLOCK_STYLE, clockStyle);
+            mStatusBarClock.setSummary(mStatusBarClock.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarAmPm) {
+            int statusBarAmPm = Integer.valueOf((String) newValue);
+            int index = mStatusBarAmPm.findIndexOfValue((String) newValue);
+            Settings.System.putInt(
+                    resolver, STATUS_BAR_AM_PM, statusBarAmPm);
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarPowerMenu) {
             String statusBarPowerMenu = (String) newValue;
             int statusBarPowerMenuValue = Integer.parseInt(statusBarPowerMenu);
             Settings.System.putInt(getActivity().getContentResolver(),
