d1c892c115e408e4560778d086b1579ec7fdf483
Crostantin <crostantin@gmail.com>
2015-04-01 12:53:47 +0200

    Status bar: add percentage battery
    thanks to @Rascarlo
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- src/com/android/settings/boost/StatusBarSettings.java
+++ src/com/android/settings/boost/StatusBarSettings.java
@@ -28,15 +28,25 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private static final String STATUS_BAR_POWER_MENU = "status_bar_power_menu";
     private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
     private ListPreference mStatusBarPowerMenu;
     private ListPreference mQuickPulldown;
+    private ListPreference mStatusBarBatteryShowPercent;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.boost_status_bar_settings);
+
+        mStatusBarBatteryShowPercent =
+                (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        int batteryShowPercent = Settings.System.getInt(getActivity().getContentResolver(), 
+		Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        mStatusBarBatteryShowPercent.setValue(String.valueOf(batteryShowPercent));
+        mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
+        mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
 
         // status bar power menu
         mStatusBarPowerMenu = (ListPreference) findPreference(STATUS_BAR_POWER_MENU);
@@ -71,6 +81,13 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                     .findIndexOfValue(statusBarPowerMenu);
             mStatusBarPowerMenu
                     .setSummary(mStatusBarPowerMenu.getEntries()[statusBarPowerMenuIndex]);
+            return true;
+        } else if (preference == mStatusBarBatteryShowPercent) {
+            int batteryShowPercent = Integer.valueOf((String) newValue);
+            int index = mStatusBarBatteryShowPercent.findIndexOfValue((String) newValue);
+            Settings.System.putInt(
+                    getActivity().getContentResolver(), Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, batteryShowPercent);
+            mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntries()[index]);
             return true;
 	} else if (preference == mQuickPulldown) {
             int statusQuickPulldown = Integer.valueOf((String) newValue);
